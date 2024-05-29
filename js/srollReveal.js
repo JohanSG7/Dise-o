4abd36ec1bb0556ed61a29819b4038e6f9c4e163
JohanSG7 <jsgiraldo2ga@gmail.com>
2024-05-28 22:11:16 -0500

    repositorio

---
+++ js/srollReveal.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+    sr.reveal('#contenedorInfo', {
+        duration: 3000
+    });
+    sr.reveal('#contenedorVideo', {
+        duration: 3000
+    });
+    sr.reveal('#tituloPrincipal', {
+        duration: 3000
+    });
+    sr.reveal('#flecha', {
+        duration: 3000
+    });
+    sr.reveal('#carouselExampleCaptions', {
+        duration: 3000
+    });
